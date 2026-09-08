0fc5103b39d9859c1c0a789c234abc7b1b991751
ManauTunjae <manau.tunjae@hotmail.com>
2026-09-08 18:37:43 +0200

    feat: hämta author från storyblok

--- src/app/authors/page.jsx
+++ src/app/authors/page.jsx
@@ -1,3 +1,12 @@
 import { getStoryblokApi } from '@/lib/storyblok/blok';
 import Link from 'next/link';
 
+export default async function AuthorsPage() {
+	const storyblokApi = getStoryblokApi();
+	const { data } = await storyblokApi.getStories({
+		version: 'draft',
+		content_type: 'author',
+		starts_with: 'authors/',
+	});
+    const authors = data.stories;
+}
